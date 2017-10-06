afb988546c44381f709d157be39e35941322aae0
Peter Wu <piterwu@outlook.com>
2017-10-06 13:27:54 +0800

    处理配置文件权限

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,13 +2,6 @@
 
 groupmod -o -g $AUDIO_GID audio
 
-if [ ! -d "/home/netease/Music/.config" ]; then
-    mkdir -p "/home/netease/Music/.config"
-fi
-if [ ! -d "/home/netease/Music/.cache" ]; then
-    mkdir -p "/home/netease/Music/.cache"
-fi
-
 if [ $GID != $(echo `id -g netease`) ]; then
     groupmod -o -g $GID netease
 fi
@@ -17,9 +10,14 @@ if [ $UID != $(echo `id -u netease`) ]; then
 fi
 chown netease:netease /home/netease/Music
 
-
 su netease <<EOF
-    echo "启动 $APP"
-    netease-cloud-music $1
-    exit 0
+if [ ! -d "/home/netease/Music/.config" ]; then
+    mkdir -p "/home/netease/Music/.config"
+fi
+if [ ! -d "/home/netease/Music/.cache" ]; then
+    mkdir -p "/home/netease/Music/.cache"
+fi
+echo "启动 $APP"
+netease-cloud-music $1
+exit 0
 EOF
